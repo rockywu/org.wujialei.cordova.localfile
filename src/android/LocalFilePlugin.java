ab3bcc87a8485fb8f9a8ad0e9400a6e4ec37e609
rockywu 吴佳雷 <rockywu@anjuke.com, wjl19890427@hotmail.com>
2015-11-27 10:08:39 +0800

    modify

--- src/android/LocalFilePlugin.java
+++ src/android/LocalFilePlugin.java
@@ -8,46 +8,53 @@
  */
 package org.wujialei.cordova.localfile;
 
-import org.apache.cordova.CordovaPlugin;
+import android.content.Context;
 
-public class LocalFilePugin extends CordovaPlugin {
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+
+public class LocalFilePlugin extends CordovaPlugin {
 
     private static final String LOG_TAG = "LocalFilePlugin";
 
     private Context mContext;
 
     private CordovaWebView webview;
-	
-	@Override
-	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-	    super.initialize(cordova, webView);
-	    // your init code here
-	    this.mContext = cordova.getActivity();
-	    this.webView = webView;
-	    
-	}
-	
-	@Override
+  
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+      super.initialize(cordova, webView);
+      // your init code here
+      this.mContext = cordova.getActivity();
+      this.webView = webView;
+      
+  }
+  
+  @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
-  //   	Log.v("fxapp","action : " + action);
-		// if (action.equals(this.deviceRegister)) {
-  //       	Log.v("fxapp","deviceRegister is Running");
-  //       	if(this.deviceRegister(args)) {
-  //       		Log.v("fxapp",this.deviceRegister + " Status.Ok");
-  //       		callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "deviceRegister is success"));
-  //       	} else {
-  //       		Log.v("fxapp",this.deviceRegister + " Status.Error");
-  //       		callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "city_id is 0 or null"));
-  //       	}
+  //    Log.v("fxapp","action : " + action);
+    // if (action.equals(this.deviceRegister)) {
+  //        Log.v("fxapp","deviceRegister is Running");
+  //        if(this.deviceRegister(args)) {
+  //          Log.v("fxapp",this.deviceRegister + " Status.Ok");
+  //          callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "deviceRegister is success"));
+  //        } else {
+  //          Log.v("fxapp",this.deviceRegister + " Status.Error");
+  //          callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "city_id is 0 or null"));
+  //        }
   //           return false;
   //       } else if(action.equals(this.networkReload)) {
-  //       	this.networkReload(args);
-  //       	return false;
+  //        this.networkReload(args);
+  //        return false;
   //       } else if(action.equals(this.displayMessage)) {
-  //       	this.displayMessage(args);
+  //        this.displayMessage(args);
   //       }
   //       return true;  // Returning false results in a "MethodNotFound" error.
+    return true;
     }
-	
-	
+  
+  
 }
