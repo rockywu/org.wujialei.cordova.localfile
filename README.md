2d11d7ad6ba6a569eacf3e91ba9269fed5590caf
rockywu 吴佳雷 <rockywu@anjuke.com, wjl19890427@hotmail.com>
2015-11-27 09:36:17 +0800

    添加local

---
+++ README.md
@@ -0,0 +1,7 @@
+#### 配置运行使用本地 file protocol
+
+__ 主要目的 __
+
+用于解决，外部网站无法访问本地资源
+
+例如：file:///android_asset/www/ 替换成 localfile://进行文件加载
---
+++ src/android/LocalFilePlugin.java
@@ -0,0 +1,53 @@
+/*
+ * 允许跨域加载本地file:///android_asset/www/下的文件
+ * 
+ * @author rockywu
+ * @email wjl19890427@hotmail.com
+ * @copyright Wujialei Inc
+ * @created 2015-07-17
+ */
+package org.wujialei.cordova.localfile;
+
+import org.apache.cordova.CordovaPlugin;
+
+public class LocalFilePugin extends CordovaPlugin {
+
+    private static final String LOG_TAG = "LocalFilePlugin";
+
+    private Context mContext;
+
+    private CordovaWebView webview;
+	
+	@Override
+	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+	    super.initialize(cordova, webView);
+	    // your init code here
+	    this.mContext = cordova.getActivity();
+	    this.webView = webView;
+	    
+	}
+	
+	@Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+  //   	Log.v("fxapp","action : " + action);
+		// if (action.equals(this.deviceRegister)) {
+  //       	Log.v("fxapp","deviceRegister is Running");
+  //       	if(this.deviceRegister(args)) {
+  //       		Log.v("fxapp",this.deviceRegister + " Status.Ok");
+  //       		callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "deviceRegister is success"));
+  //       	} else {
+  //       		Log.v("fxapp",this.deviceRegister + " Status.Error");
+  //       		callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "city_id is 0 or null"));
+  //       	}
+  //           return false;
+  //       } else if(action.equals(this.networkReload)) {
+  //       	this.networkReload(args);
+  //       	return false;
+  //       } else if(action.equals(this.displayMessage)) {
+  //       	this.displayMessage(args);
+  //       }
+  //       return true;  // Returning false results in a "MethodNotFound" error.
+    }
+	
+	
+}
